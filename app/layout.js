5d4cab8d63a8c72aefa3123333edfd655dd0c45c
Faisal <faisal.rokomari@gmail.com>
2026-08-24 06:08:25 +0600

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Rokomari AutoFlow",
+  description: "Schedule and manage your Pinterest pins",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
